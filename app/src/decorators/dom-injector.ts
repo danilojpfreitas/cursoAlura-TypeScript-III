6fb466a8cb1af3c9392766f46c6b3499353ac4e4
danilojpfreitas <danilojpfreitas@gmail.com>
2022-08-20 18:58:46 -0300

    Parte 3

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,6 +1,6 @@
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
-
+        console.log(`Modificante protype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
         const getter = function() {
             const elemento = document.querySelector(seletor);
             return elemento;
